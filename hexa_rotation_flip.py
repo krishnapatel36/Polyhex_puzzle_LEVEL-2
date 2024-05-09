e669704228695decf8a100906c806197edf76c36
Krishna Patel <157887090+krishnapatel36@users.noreply.github.com>
2024-05-09 23:04:59 +0530

    Add files via upload

---
+++ hexa_rotation_flip.py
@@ -0,0 +1,47 @@
+P1="""
+ I FF L
+I     L
+"""
+P2="""
+ II FF  L  
+II  FF LL
+       L
+"""
+P3="""
+I FFFF    L
+I        L
+I       L
+I      L
+"""
+P4="""
+I    F  LL  P NNN   T  U VVV WW   X  Y    Z
+II FFF  L  PP N    T   U  V  W  XX  YYY  ZZ
+I      L    P     TT  UU     W  X       Z
+"""
+P5="""
+ I    F
+ II FF
+I    F
+"""
+P6="""
+II  FF  LL  P N N   T 
+ I F F L   P  NN    T
+I      L   PP     TT  
+"""
+P7="""
+II  FFF   L P     N   T   UU  V  W XXX Y      Z
+ I F     L  P  NNN    T  U   V   W   X YYY   Z
+ I      L   PP       T  U    V   W         ZZ
+        L           T        V  W
+"""
+P8="""
+  I F    LL PP    N    T
+ I  FF LL    PP   N  TT
+ I   F           N  T
+I                N
+"""
+P9="""
+I FFF   L
+I      L
+I     L
+"""
